4c4a4c57bc9f32e48d8ebd2051540eb1676dd8da
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-20 16:37:19 +0200

    done and working

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-let name = prompt("What's your name?");
-console.log(name);
+// Chiedi all’utente il suo nome,
+let firstName = prompt("What's your name?");
+
+// poi chiedi il suo cognome,
+let lastName = prompt("What's your last name?");
+
+// poi chiedi il suo colore preferito
+let favColor = prompt("What's your favourite color?");
+
+// Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21
+document.getElementById("pw_gen").innerHTML = `La password assegnata è: ${firstName}${lastName}${favColor}21`;
